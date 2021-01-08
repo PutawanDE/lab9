c4ed40d7b331484f801cc278d535363772038fc1
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2021-01-08 16:00:47 +0700

    Finish lab8

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,25 +3,46 @@ using namespace std;
 
 int main(){
 	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
-	cout << "Please input grade of each student (A-F) or input 0 to exit.";
+	int students;
+	char grade;
+	cout << "Please input grade of each student (A-F) or input 0 to exit." << '\n';
 	do{
-		cout << "Student [" << "]:";
+		cout << "Student [" << students + 1 << "]: ";
 		cin >> grade; //The loop must be terminated when grade = '0'
-		if(true) // if grade is A
-			//Do something
-		}else if(true) // if grade is B
-			//Do something
-		//and so on ... for grade = C, D, F	
-		}else{ // grade is wrong input
-			//Do something
-		} 
+		if (grade == '0') break;
+		switch (grade){
+			case 'A':
+				count[0]++;
+				students++;
+				break;
+			case 'B':
+				count[1]++;
+				students++;
+				break;
+			case 'C':
+				count[2]++;
+				students++;
+				break;
+			case 'D':
+				count[3]++;
+				students++;
+				break;
+			case 'F':
+				count[4]++;
+				students++;
+				break;
+			default:
+				cout << "Wrong input. Please input again." << '\n';
+				break;
+		}
 	}while(true);
 	
-	
-	cout << "In total ? students.";
+	cout << "In total " << students << " students." << '\n';
 	cout << "A = " << count[0] <<", ";
-	cout << "B = " << count[1] <<", ";	
-	//	and so on ... for grade = C, D, F	
+	cout << "B = " << count[1] <<", ";
+	cout << "C = " << count[2] <<", ";
+	cout << "D = " << count[3] <<", ";	
+	cout << "F = " << count[4];
 	
 	return 0;
 }
--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,35 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    string textline;
+    int n = 0;
+    float sum;
+    float sumX_sq;
+    float mean;
+    float sd;
+
+    while(getline(source, textline)){
+        float x = atof(textline.c_str());
+        sum += x;
+        sumX_sq += (x * x);
+        n++;
+    }
+
+    mean = sum / n;
+    sd = sqrt((sumX_sq/n)-(mean*mean));
+    
+    cout << "Number of data = " << n << '\n';
+    cout << setprecision(3);
+    cout << "Mean = " << mean << '\n';
+    cout << "Standard deviation = " << sd;
+    return 0;
+}
